479bd49b78827c7b8d92fe04e62d0911cc2c5b9e
Nikhil Narvekar <nikhil.narvekar.712@gmail.com>
2020-11-26 00:18:59 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo1.png';
 import './App.css';
-import {Section} from './Section.js';
+import Section from './Section.js';
 import {Navbar} from './Navbar.js';
 import profilePic from './homepfp.png';
-import Section from './Section.js';
 import background from './bgd2.png';
 
 function App() {
 
-
+  let h1Style = {color:'white'};
 
   return (
     <div className="App">
@@ -17,11 +16,16 @@ function App() {
         
       <Section image={background} height='400px'>
         <img src={profilePic} width='200px' height='200px'></img>
+        <h1 style={h1Style}>
+          Data Science
+        </h1>
       </Section>
       <Section color='#2c2c2c'>
         <h1 class='text-lg'>About</h1>
         <p></p>
       </Section>
+
+
       <header className="App-header">
       
       
